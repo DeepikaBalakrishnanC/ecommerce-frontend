447fc85eb9f02f416e7b2b00203bec6ed1c4a956
Deepika C <deepikac@Deepikas-MacBook-Air.local>
2026-04-27 23:48:47 -0700

    Added responsive header and navbar

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,6 @@
-console.log("Website Loaded");
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
